ce3c6971e0c74ada7b316732874867589570baef
Sebastian Arriola <sebdeveloper6952@gmail.com>
2020-07-26 16:50:30 -0600

    initial commit.

---
+++ README.md
@@ -0,0 +1,9 @@
+# UVG - Proyectos de TI
+
+Proyecto 1
+
+## Instrucciones
+1. git clone
+2. cd <folder_donde_se_clono>
+3. flutter pub get
+4. flutter run
---
+++ android/app/src/main/kotlin/com/sebdeveloper6952/flutter_proyecto_1/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.sebdeveloper6952.flutter_proyecto_1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
