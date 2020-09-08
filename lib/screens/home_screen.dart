32072ea65feea2187d5e41a313de83904e4011b9
javiercarpio57 <javierrr.carpio57@gmail.com>
2020-09-08 04:02:57 -0600

    Menu con tarjetas de lecturas y juego

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -13,12 +13,78 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        leading: Container(),
-      ),
-      body: Center(
-        child: Text('Home Screen'),
-      ),
-    );
+        appBar: AppBar(
+          leading: Container(),
+        ),
+        body: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Card(
+                child: InkWell(
+                  splashColor: Colors.blue.withAlpha(30),
+                  onTap: () {
+                    print('Card tapped.');
+                  },
+                  child: Container(
+                      width: 300,
+                      height: 100,
+                      child: Center(
+                          child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                            Text.rich(
+                              TextSpan(
+                                  text: 'Lecturas',
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 20)),
+                            ),
+                            Text.rich(
+                              TextSpan(
+                                  text:
+                                      'Realiza las lecturas sobre el libro de PMBOK',
+                                  style: TextStyle(fontSize: 12)),
+                            )
+                          ]))),
+                ),
+                elevation: 5,
+              ),
+              Card(
+                child: InkWell(
+                  splashColor: Colors.blue.withAlpha(30),
+                  onTap: () {
+                    print('Card tapped.');
+                  },
+                  child: Container(
+                      width: 300,
+                      height: 100,
+                      child: Center(
+                          child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                            Text.rich(
+                              TextSpan(
+                                  text: 'Jueguito',
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 20)),
+                            ),
+                            Text.rich(
+                              TextSpan(
+                                  text:
+                                      'Pon a prueba tu conocimiento sobre PMBOK',
+                                  style: TextStyle(fontSize: 12)),
+                            )
+                          ]))),
+                ),
+                elevation: 5,
+              ),
+            ],
+          ),
+        ));
   }
 }
